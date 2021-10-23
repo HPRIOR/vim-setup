d8a6a2ca17b7c008155bf5112e7d0ccf2e7db7ad
Harry Prior <harryprior@hotmail.co.uk>
2021-10-23 10:21:13 +0100

    fix luasnip issue

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,13 +1,11 @@
   -- Setup nvim-cmp.
   local cmp = require'cmp'
+  local luasnip = require'luasnip'
 
   cmp.setup({
     snippet = {
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+         luasnip.lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
     mapping = {
@@ -15,7 +13,10 @@
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
       ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -38,10 +39,7 @@
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+      { name = 'luasnip' }, -- For luasnip users.
     }, {
       { name = 'buffer' },
     })
@@ -49,6 +47,7 @@
 
  -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- each server requires to it's capabilites to be setup here
   require('lspconfig')['tsserver'].setup {
     capabilities = capabilities
   }
