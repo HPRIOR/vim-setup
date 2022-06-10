cfee6e6966cee6095ab6d47c38a4c911b7ba4616
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-06-10 09:40:18 +0100

    update

---
+++ lua/lsp-sig.lua
@@ -0,0 +1,9 @@
+require("lsp_signature").setup({
+    bind = true,
+    handler_opts = {
+        border = "rounded"
+    },
+    hint_enable = false,
+    hint_prefix = "",
+    hi_parameter = "LspSignatureActiveParameter",
+})
