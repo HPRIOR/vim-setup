128eaa73deb78bd375a817fd6f44476abfc40e13
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-06-22 12:57:03 +0100

    slightly better directory opening

--- lua/filetree.lua
+++ lua/filetree.lua
@@ -1,14 +1,14 @@
 require('nvim-tree').setup {
   disable_netrw       = true,
   hijack_netrw        = true,
-  open_on_setup       = false,
+  open_on_setup       = true,
   ignore_ft_on_setup  = {},
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
   update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
+    enable = false,
+    auto_open = false,
   },
   diagnostics = {
     enable = true,
