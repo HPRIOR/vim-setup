902ffb3e5a4c9d9fc5ae5d14d5efd95122484a70
Harry Prior <harryprior@hotmail.co.uk>
2022-07-21 15:13:22 +0100

    update

--- lua/filetree.lua
+++ lua/filetree.lua
@@ -1,6 +1,6 @@
 require('nvim-tree').setup {
-  disable_netrw       = false,
-  hijack_netrw        = false,
+  disable_netrw       = true,
+  hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
   open_on_tab         = true,
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,12 +2,12 @@
 -- Set Default Prefix.
 -- Note: You can set a prefix per lsp server in the lv-globals.lua file
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
+    vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
     signs = true,
     underline = true,
     priority = 1
-  }
+}
 )
 
 
@@ -42,32 +42,45 @@ vim.lsp.protocol.CompletionItemKind = {
 local lsp_installer = require("nvim-lsp-installer")
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local on_attach_all = function (client, _)
+local on_attach_all = function(client, _)
     -- add signiture help from lsp_signature
     -- require("lsp_signature").on_attach()
     require('illuminate').on_attach(client)
- end
+end
 
-local function setup_lua (opts)
-        opts.settings = {
-             Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = vim.split(package.path, ';'),
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = {
-              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+local function setup_lua(opts)
+    opts.settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';'),
             },
-          },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                },
+            },
+        }
+    }
+end
+
+local function setup_typescript(opts)
+    opts.settings = {
+        typescript = {
+            format = {
+                indentSize = 2,
+                tabsize = 2,
+                semicolons = 'insert'
+
+            }
         }
     }
 end
@@ -79,12 +92,10 @@ lsp_installer.on_server_ready(function(server)
     if server.name == "sumneko_lua" then
         setup_lua(opts)
     end
+    if server.name == "tsserver" then
+        setup_typescript(opts)
+    end
 
     server:setup(opts)
     vim.cmd [[ do User LspAttachBuffers ]]
 end)
-
-
-
-
-
